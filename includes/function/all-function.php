ebeaf017a5659e3b023938f10207957aca98aad7
KR!$$H <48906449+KrisshRp@users.noreply.github.com>
2024-08-02 14:37:28 +0530

    clean

--- includes/function/all-function.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*get_template_part('includes/function/site','enqueue');
-get_template_part('includes/function/site','pagination');
-get_template_part('includes/function/custom','post');
-get_template_part('includes/function/theme','support');
-get_template_part('includes/function/site','widgets');
-
-get_template_part('includes/function/wp','bootstrap','navwalker');*/
